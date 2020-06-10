e6e08dcd07a8d6251211c58defa74d705dc51766
José Saavedra <josesaavedra@MacBook-Pro.local>
2020-06-10 00:43:56 -0500

    first commit

---
+++ arrow.py
@@ -0,0 +1,22 @@
+#python print practice (first steps...)
+#created by Jose Saavedra
+#10/06/20
+#12:43 a.m.
+
+
+print()
+print("This:")
+print()
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+print()
+print("To this:")
+print()
+print("    *", "\n   * *", "\n  *   *", "\n *     *","\n***   ***", "\n  *   *" *2, "\n  *****")
+print()
